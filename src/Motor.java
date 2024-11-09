95c95e6a041ab1a87a70dc053449acf025e55a74
Valen2525 <juroseror@unal.edu.co>
2024-11-08 19:36:47 -0500

    Add files via upload

---
+++ src/Motor.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+
+    void cambiarRegistro(int numeroRegistro){
+        this.registro = numeroRegistro;
+    }
+
+    void asignarTipo(String tipoMotor){
+        if (tipoMotor == "electrico" || tipoMotor == "gasolina"){
+            this.tipo = tipoMotor;
+        }
+    }
+
+}
